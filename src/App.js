af1952bff832354ee62c8565edf7e02cd7e75c93
Joanna Qian <honggzb@users.noreply.github.com>
2023-12-20 16:58:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,87 @@
-import React, { useEffect } from 'react';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import React, { useEffect } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+//引入性能监视器stats.js,显示帧率
+import Stats from "three/addons/libs/stats.module.js";
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+
+//创建stats对象
+const stats = new Stats();
+//Stats.domElement:web页面上输出计算结果,一个div元素
+document.body.appendChild(stats.domElement);
 
 let camera = null; //相机
 let scene = null; //场景
 let renderer = null; //渲染器
 let container = null; //three挂载对象
-let controls = null; //控件
+//let controls = null; //控件
 
 function App() {
   //let controls = null; //控件
-    //初始加载场景
-    const createtHREE = () => {
-      //新建场景
-      scene = new THREE.Scene();
-      //新建一个相机（相当于眼睛，只有有了眼睛才能视物）
-      camera = new THREE.PerspectiveCamera( 75, ((3 / 4) * window.innerWidth) / ((3 / 4) * window.innerHeight), 0.1, 1000 );
-      //将相机放入合适的位置
-      camera.position.set(30, 30, 50)
-      camera.lookAt(0,0,0)
-      // 生成渲染器
-      renderer = new THREE.WebGLRenderer();
-      renderer.setSize((3 / 4) * window.innerWidth, (3 / 4) * window.innerHeight);
-      renderer.setClearColor('#888', 0.5); //背景颜色
-      container = document.getElementById('container');
-      container.appendChild(renderer.domElement); //生成的渲染的实例, 这个要放到对应的dom容器里面
-      controls = new OrbitControls(camera, renderer.domElement);
-      addGeo();
-      animate();
-    };
-    //生成几何体
-    const addGeo = () => {
-      const geometry = new THREE.BoxGeometry(30, 30, 30);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-    };
-    function animate() {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-    }
-    useEffect(() => {
-      createtHREE();
-    }, []);
+  //初始加载场景
+  const createtHREE = () => {
+    //新建场景
+    scene = new THREE.Scene();
+    //新建一个相机（相当于眼睛，只有有了眼睛才能视物）
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 
-  return (
-    <div id="container"></div>
-  );
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+    directionalLight.position.set(0, 1, 0);
+    scene.add(directionalLight);
+
+    //辅助观察的坐标系
+    const axesHelper = new THREE.AxesHelper(50);
+    scene.add(axesHelper);
+
+    // Make the camera further from the cube so we can see it better
+    camera.position.set(0, 1, 2);
+    camera.lookAt(0, 0, 0);
+    // 生成渲染器
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container = document.getElementById("container");
+    container.appendChild(renderer.domElement); //生成的渲染的实例, 这个要放到对应的dom容器里面
+    //controls = new OrbitControls(camera, renderer.domElement);
+    addModel();
+    animate();
+  };
+
+  //生成几何体;
+  const addModel = () => {
+    // Load 3D model in GLB format
+    const loader = new GLTFLoader();
+    let model;
+    // model must put in public folder
+    loader.load("building.glb", (gltf) => {
+        model = gltf.scene;
+        model.scale.set(3, 3, 3);
+        model.position.z = -2;
+        model.position.x = 2;
+        model.position.y = 1;
+        scene.add(gltf.scene);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+
+  function animate() {
+    stats.update();      //渲染循环中执行stats.update()来刷新时间
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+
+  useEffect(() => {
+    if(!scene) { createtHREE();}
+  }, []);
+
+  return <div id="container"></div>;
 }
 
 export default App;
